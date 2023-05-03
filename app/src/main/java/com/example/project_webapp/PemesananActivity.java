09eb798f441598f5002e15796d233bb22330f263
Daffaizaaz <117889603+Daffaizath12@users.noreply.github.com>
2023-05-03 22:36:14 +0700

    pemesanan

--- app/src/main/java/com/example/project_webapp/PemesananActivity.java
+++ app/src/main/java/com/example/project_webapp/PemesananActivity.java
@@ -1,6 +1,7 @@
 package com.example.project_webapp;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class PemesananActivity extends AppCompatActivity {
 
@@ -17,12 +24,25 @@ public class PemesananActivity extends AppCompatActivity {
     AutoCompleteTextView autoCompleteTextView, pembayaran;
     ArrayAdapter<String> adapterItem, adapterItemPembayaran;
 
+    EditText txttanggal;
+    DatePickerDialog datePickerDialog;
+    SimpleDateFormat dateFormatter;
+
     Button backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pemesanan);
+
+        txttanggal = (EditText) findViewById(R.id.txttanggal);
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+        txttanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateDialog();
+            }
+        });
 
 
         autoCompleteTextView = findViewById(R.id.auto_complete_text);
@@ -49,5 +69,19 @@ public class PemesananActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void showDateDialog() {
+        Calendar calendar = Calendar.getInstance();
+
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                txttanggal.setText(dateFormatter.format(newDate.getTime()));
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
     }
 }

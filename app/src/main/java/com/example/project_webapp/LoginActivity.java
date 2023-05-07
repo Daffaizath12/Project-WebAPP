0c8d37c761bfaeb3540d4a167db9855da011f6c8
ElaErwita <ellaaaa183@gmail.com>
2023-05-07 08:06:11 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/project_webapp/LoginActivity.java
+++ app/src/main/java/com/example/project_webapp/LoginActivity.java
@@ -1,49 +1,56 @@
 package com.example.project_webapp;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-
+import androidx.appcompat.app.AlertDialog;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
-
-
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText Txtusername, Txtpassword;
-    Button btnLogin;
+    private static final int REQUEST_CODE = 10;
+    EditText Username, Password;
+    Button startBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Username = (EditText) findViewById(R.id.Username);
+        Password = (EditText) findViewById(R.id.Password);
+        startBtn = (Button) findViewById(R.id.startBtn);
 
-        Txtusername = (EditText) findViewById(R.id.username);
-        Txtpassword = (EditText) findViewById(R.id.password);
-        btnLogin = (Button) findViewById(R.id.startBtn);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+                btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = Txtusername.getText().toString();
-                String password = Txtpassword.getText().toString();
+                String usernameKey = Username.getText().toString();
+                String passwordKey = Password.getText().toString();
+                if (usernameKey.equals("appank") && passwordKey.equals("1")){
+                    //jika login berhasil
+                    Toast.makeText(getApplicationContext(), "Login Berhasil",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    //  MainActivity.this.startActivity(intent); aktifkan jika menghapus bagian bawah
+                    // Data username di ambil menuju ke activity2
+                    String value = Username.getText().toString();
+                    intent.putExtra("yourkey1", value);
+                    startActivityForResult(intent, REQUEST_CODE);
 
-                // Contoh validasi username dan password
-                if (username.equals("admin") && password.equals("password")) {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(LoginActivity.this, "Username atau password salah", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    //jika login gagal
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage("Username atau Password Anda salah!")
+                            .setNegativeButton("Ulangi", null).create().show();
                 }
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
             }
         });
-
     }
+
+
 }

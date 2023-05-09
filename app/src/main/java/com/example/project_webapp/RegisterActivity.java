fce79a1120376bbb26744d4205cedbfef86ae59a
ElaErwita <ellaaaa183@gmail.com>
2023-05-09 08:38:28 +0700

    Update RegisterActivity.java

--- app/src/main/java/com/example/project_webapp/RegisterActivity.java
+++ app/src/main/java/com/example/project_webapp/RegisterActivity.java
@@ -1,21 +1,127 @@
 package com.example.project_webapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
-import com.example.project_webapp.RegisterActivity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.project_webapp.LoginActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
+<<<<<<< Updated upstream
 //    lateinit var binding: RegisterActivity
+=======
+    EditText username, email, passsword;
+    Button startBtn;
+    ProgressDialog progressDialog;
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+<<<<<<< Updated upstream
 //        binding.textToRegister.setOnClickListener {
 //            Intent(Intent(this, RegisterActivity::class.java))
 //        }
+=======
+        username = (EditText) findViewById(R.id.username);
+        email = (EditText) findViewById(R.id.email);
+        passsword = (EditText) findViewById(R.id.password);
+        startBtn = (Button) findViewById(R.id.startBtn);
+        progressDialog = new ProgressDialog(RegisterActivity.this);
+
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sUsername = username.getText().toString();
+                String sEmail = email.getText().toString();
+                String sPassword = passsword.getText().toString();
+
+                if (sPassword.equals(sPassword) && !sPassword.equals("")) {
+                    CreateDataToServer(sUsername, sEmail, sPassword);
+                    Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Gagal! Pasword tidak cocok!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+>>>>>>> Stashed changes
     }
+
+//    public void CreateDataToServer(final String username, final String email, final String password) {
+//        if (checkNetworkConnection()) {
+//            progressDialog.show();
+//            StringRequest stringRequest = new StringRequest(Request.Method.POST, DbContract.SERVER_REGISTER_URL,
+//                    new Response.Listener<String>() {
+//                        @Override
+//                        public void onResponse(String response) {
+//                            try {
+//                                JSONObject jsonObject = new JSONObject(response);
+//                                String resp = jsonObject.getString("server_response");
+//                                if (resp.equals("[{\"status\":\"OK\"}]")) {
+//                                    Toast.makeText(getApplicationContext(), "Registrasi Berhasil", Toast.LENGTH_SHORT).show();
+//                                } else {
+//                                    Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
+//                                }
+//                            } catch (JSONException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//
+//                }
+//            }) {
+//                @Override
+//                protected Map<String, String> getParams() throws AuthFailureError {
+//                    Map<String, String> params = new HashMap<>();
+//                    params.put("username", username);
+//                    params.put("email", email);
+//                    params.put("password", password);
+//                    return params;
+//                }
+//            };
+//
+//            VolleyConnection.getInstance(RegisterActivity.this).addToRequestQue(stringRequest);
+//
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    progressDialog.cancel();
+//                }
+//            }, 2000);
+//        } else {
+//            Toast.makeText(getApplicationContext(), "Tidak ada koneksi internet", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    public boolean checkNetworkConnection() {
+//        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+//        return (networkInfo != null && networkInfo.isConnected());
+//    }
 }

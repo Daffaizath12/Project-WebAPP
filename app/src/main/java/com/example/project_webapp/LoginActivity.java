4ab6d83e907fac31711ff6c54db0f139942bb02f
Daffaizath12 <117889603+Daffaizath12@users.noreply.github.com>
2023-05-12 02:33:09 +0700

    fix Bug Login & Register

--- app/src/main/java/com/example/project_webapp/LoginActivity.java
+++ app/src/main/java/com/example/project_webapp/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.project_webapp;
 
+import static com.android.volley.VolleyLog.TAG;
+
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,60 +18,99 @@ import android.widget.Toast;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.example.project_webapp.Config.DBHelper;
+import com.android.volley.toolbox.StringRequest;
+import com.example.project_webapp.Config.VolleySingleton;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String URL = "http://bernadylandslawu.wsmif3a.id/api/login_api.php";
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonLogin;
 
-    EditText Username, Password;
-    Button startBtn;
-    private TextView registerbtn;
+    private TextView registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Username = findViewById(R.id.username);
-        Password = findViewById(R.id.password);
-        TextView registerbtn = findViewById(R.id.register_btn);
-        Button startBtn = findViewById(R.id.startBtn);
+        // Inisialisasi view
+        editTextEmail = findViewById(R.id.username);
+        editTextPassword = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.startBtn);
+        registerBtn = findViewById(R.id.register_btn);
 
-
-
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        // Set listener untuk button login
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = Username.getText().toString().trim();
-                String password = Password.getText().toString().trim();
-
-                if (!email.isEmpty() && !password.isEmpty()) {
-                    String url = URL + "?email=" + email + "&password=" + password;
-
-                    DBHelper dbHelper = DBHelper.getInstance(LoginActivity.this);
-
-                    dbHelper.executeQuery(url, new DBHelper.VolleyCallback() {
-                        @Override
-                        public void onSuccess(String result) {
-                            if (result.equals("success")) {
-                                // Login berhasil, lakukan tindakan sesuai kebutuhan
-                            } else {
-                                // Login gagal, tampilkan pesan kesalahan
-                                Toast.makeText(LoginActivity.this, "Email atau password salah", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(VolleyError error) {
-                            Toast.makeText(LoginActivity.this, "Terjadi kesalahan", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                } else {
-                    Toast.makeText(LoginActivity.this, "Email dan password harus diisi", Toast.LENGTH_SHORT).show();
-                }
+                login();
             }
         });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pindah ke RegisterActivity jika pengguna belum memiliki akun
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void login() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // Validasi email dan password
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        // Kirim request login ke server menggunakan Volley
+        String url = "http://bernadylandslawu.wsmif3a.id/";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle response dari server setelah login berhasil
+                        // Misalnya, simpan token di SharedPreferences atau langsung pindah ke halaman utama
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                        Toast.makeText(getApplicationContext(), "Login berhasil", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleySingleton.logError("Error on login request", error);
+                Toast.makeText(getApplicationContext(), "Login gagal: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        // Tambahkan request ke queue Volley
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
 }
--- app/src/main/java/com/example/project_webapp/RegisterActivity.java
+++ app/src/main/java/com/example/project_webapp/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,61 +16,112 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.example.project_webapp.Config.DBHelper;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.project_webapp.Config.VolleySingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private static final String URL = "http://bernadylandslawu.wsmif3a.id/api/register_api.php";
-
-    private EditText Namalengkap, Username, Email, Password;
-
-    Button registerBUtton;
+    private EditText editTextName, editTextEmail, editTextPassword;
+    private Button buttonRegister;
+    private  TextView loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Namalengkap = findViewById(R.id.nama_lengkap);
-        Username = findViewById(R.id.username);
-        Email = findViewById(R.id.email);
-        Password = findViewById(R.id.password);
+        // Inisialisasi view
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        editTextName = findViewById(R.id.username);
+        buttonRegister = findViewById(R.id.startBtn);
+        loginBtn = findViewById(R.id.loginbtn);
 
-        Button registerButton = findViewById(R.id.startBtn);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        // Set listener untuk button register
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = Namalengkap.getText().toString().trim();
-                String username = Username.getText().toString().trim();
-                String email = Email.getText().toString().trim();
-                String password = Password.getText().toString().trim();
-
-                if (!name.isEmpty() && !username.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
-                    String url = URL + "?name=" + name + "&username" + username + "&email=" + email + "&password=" + password;
-
-                    DBHelper dbHelper = DBHelper.getInstance(RegisterActivity.this);
-
-                    dbHelper.executeQuery(url, new DBHelper.VolleyCallback() {
-                        @Override
-                        public void onSuccess(String result) {
-                            if (result.equals("success")) {
-                                // Registrasi berhasil, lakukan tindakan sesuai kebutuhan
-                            } else {
-                                // Registrasi gagal, tampilkan pesan kesalahan
-                                Toast.makeText(RegisterActivity.this, "Registrasi gagal, silakan coba lagi", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(VolleyError error) {
-                            Toast.makeText(RegisterActivity.this, "Terjadi kesalahan", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                } else {
-                    Toast.makeText(RegisterActivity.this, "Semua kolom harus diisi", Toast.LENGTH_SHORT).show();
-                }
+                register();
             }
         });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pindah ke RegisterActivity jika pengguna belum memiliki akun
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void register() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+
+        // Validasi email, password, dan name
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(name)) {
+            editTextName.setError("Name is required");
+            editTextName.requestFocus();
+            return;
+        }
+
+        // Kirim request registrasi ke server menggunakan Volley
+        String url = "http://bernadylandslawu.wsmif3a.id/";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle response dari server setelah registrasi berhasil
+                        // Misalnya, menampilkan pesan registrasi berhasil atau langsung pindah ke halaman login
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                        Toast.makeText(getApplicationContext(), "Registration successful", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error dari server saat registrasi gagal
+                // Misalnya, menampilkan pesan error pada EditText atau menampilkan dialog error
+                Toast.makeText(getApplicationContext(), "Registration failed: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                params.put("name", name);
+                return params;
+            }
+        };
+
+        // Tambahkan request ke queue Volley
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
 }

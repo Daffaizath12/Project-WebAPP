bf79e904e70aecc1d773b634ea9e976efd7db9d8
Daffaizath12 <117889603+Daffaizath12@users.noreply.github.com>
2023-05-17 00:37:42 +0700

    update fragment_home

--- app/src/main/java/com/example/project_webapp/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/project_webapp/Fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.project_webapp.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.project_webapp.Adapter.ItemsAdapter;
 import com.example.project_webapp.Adapter.ItemsDomain;

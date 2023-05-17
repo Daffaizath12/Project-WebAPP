cc6fd5f00990ee5b9350915cde931d78d5b30dc1
ferinaintan <ferinaintan435@gmail.com>
2023-05-17 13:27:51 +0700

    ela,ferina

---
+++ app/src/main/java/com/example/project_webapp/PembayaranDetail.java
@@ -0,0 +1,14 @@
+package com.example.project_webapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PembayaranDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran_detail);
+    }
+}
